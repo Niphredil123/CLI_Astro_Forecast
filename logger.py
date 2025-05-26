1b70b7f8360b1c527c597b6772d98bf98acba2d1
Niphredil123 <168342465+Niphredil123@users.noreply.github.com>
2025-05-26 13:04:41 +0100

    Refactored logging function to include rotating file handling and return logger

--- logger.py
+++ logger.py
@@ -5,25 +5,56 @@ import configparser
 import logging
 import os
 
+from logging.handlers import RotatingFileHandler
+
+
+###############################################################################
+# VARIABLES
+###############################################################################
+# Load config file and log details
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+#Load variable from
+log_folder = config['logging']['log_folder']
+log_file = config['logging']['log_file']
+log_level = config['logging']['log_level'].upper()
+max_size = int(config['logging']['log_max_size'])
+backup_count = int(config['logging']['log_backup_count'])
+log_format = config['logging']['log_format']
+
+# Check logging folder exists
+log_path = os.path.join(log_folder, log_file)
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
 
 ###############################################################################
 # LOGGING SET UP
 ###############################################################################
-def logging_setup():
-    """ Function to setup and configure logging."""
-    # Load config and set lof file and level
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+def logging_setup(name: str):
+    """
+    Function to setup logging.
+    Returns a configured logger with rotating file handling.
+    """
+    # Create logger
+    logger = logging.getLogger(name)
 
-    log_file = config['logging']['log_file']
-    log_level = config['logging']['log_level'].upper()
+    # If loop to prevent adding multiple handlers
+    if not logger.handlers:
+        logger.setLevel(getattr(logging, log_level, logging.INFO))
 
-    # Check logging folder exists
-    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        # Setting up file handler to rotate through logs
+        file_handler =RotatingFileHandler(log_path, maxBytes=max_size,
+                                        backupCount=backup_count)
 
-    # Set logging configuration
-    logging.basicConfig(
-        filename=log_file,
-        level=getattr(logging, log_level),
-        format='%(asctime)s - %(levelname)s - %(message)s - %(name)s - %(funcName)s'
-    )
+        # Setting log format and applying to file handler
+        file_handler.setFormatter(logging.Formatter(log_format))
+        file_handler.setLevel(getattr(logging, log_level, logging.INFO))
+
+        # add handler
+        logger.addHandler(file_handler)
+
+        # Prevent log messages being passed to root logger
+        logger.propagate = False
+
+    return logger
